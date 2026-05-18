8e8dd5d2ef6d01ee8144d4c5ddd7d2b190306301
Hassan Mohamed Said <saidhassano658@gmail.com>
2026-05-18 20:36:58 +0300

    add files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+ <div style="padding: 2rem;">
+    <h1>Vue.js Fundamentals</h1>
+    <p>This is our learning project. We'll build bakery features here.</p>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
